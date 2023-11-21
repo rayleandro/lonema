6dc2554a8df3755f1da3d9679e1175d66544dd69
rayleandro <raymo.andro@gmail.com>
2023-11-21 14:23:36 +0800

    [BROKEN] attempt at using validators for world modeling

--- src/system-tok.js
+++ src/system-tok.js
@@ -1,14 +1,23 @@
 /* parser-tok.js
    the toki pona implementation of the lon'ema system.
 */
-
+"use strict";
 
 // -: utility
 // --------------------------------------------------------------------------------------------- //
 
-
+const __ = null;
 function _flattenString(str) {
    return str.match(/\S+/g).join(' ');
+}
+function _first(tup) {
+   return tup[0];
+}
+function _second(tup) {
+   return tup[1];
+}
+function _last(array) {
+   return array[array.length - 1];
 }
 function _isDefined(o) {
    return o !== undefined;
@@ -16,11 +25,85 @@ function _isDefined(o) {
 function _isntNull(o) {
    return o !== null;
 }
-function _fst(tup) {
-   return tup[0];
+function _isFunction(x) {
+   return typeof x == 'function';
 }
-function _snd(tup) {
-   return tup[1];
+function _isString(x) {
+   return typeof x == 'string';
+}
+function _isBoolean(x) {
+   return x === true || x === false;
+}
+function _isntFunction(x) {
+   return !(typeof x == 'function');
+}
+function _isntString(x) {
+   return !(typeof x == 'string');
+}
+function _isntBoolean(x) {
+   return !(x === true || x === false);
+}
+function _isArray(obj) {
+   return obj instanceof Array;
+}
+function _isNullish(x) {
+   return x === undefined || x === null;
+}
+function _assert(x, name) {
+   if (x === undefined) {
+      throw new Error(`${name} is undefined!`)
+   }
+}
+
+function _validate(template, obj) {
+   for (const key of template) {
+      const checker = template[key];
+      if (_isFunction(checker)) {
+         if (!checker(obj[key])) {
+            throw new Error(`${key} doesn't pass predicate ${checker.name}`);
+         }
+      } else if (_isArray(checker) && checker.length == 2) {
+         const defaulter = checker[0];
+         const checkerFunc = checker[1];
+         obj[key] = defaulter(obj[key]);
+         if (!checker(obj[key])) {
+            throw new Error(`${key} doesn't pass predicate ${checkerFunc.name}`);
+         }
+      } else {
+         throw new Error();
+      }
+   }
+   return obj;
+}
+
+
+// -: tagged templates
+// --------------------------------------------------------------------------------------------- //
+
+
+/* zip the strings array and the values array. However, the strings array always
+   has 1 more length than values, and string[0] and string[-1] should bookend
+   the reconstituted string. 
+*/
+
+function _reconstituteString(strings, values) {
+   const reconstitution = [];
+   values.forEach((__, ind) => {
+      reconstitution.push(strings[ind]);
+      reconstitution.push(values[ind]);
+   });
+
+   reconstitution.push(_last(strings));
+}
+
+function _(strings, ...values) {
+   const full = _reconstituteString(strings, values);
+   const words = full.match(/\S+/g);
+   if (_isDefined(words)) {
+      return words.join(' ');
+   } else {
+      return '';
+   }
 }
 
 
@@ -82,10 +165,59 @@ if (CONFIG.onNodeConsole) {
 } else if (CONFIG.onWebPlayer) {
    // TODO web player implementation
 } else {
-   throw Error(
+   throw new Error(
       `one of these CONFIG properties should be set to true:`
       + ` onNodeConsole, onWebConsole, onWebPlayer`)
 }
+
+
+// -: world
+// --------------------------------------------------------------------------------------------- //
+
+
+const _entityValidator = {
+   descr: _isFunction,
+   long: _isFunction,
+   short: _isFunction,
+   matches: ma => ma.every(m => _isFunction(m)),
+   isObvious: [
+      k => _isNullish(k)? true : k,
+      _isBoolean,
+   ],
+};
+
+function O(obj) {
+   obj = _validate(_entityValidator, obj);
+   obj.parent = null;
+   obj.children = new Set();
+   return obj;
+}
+
+const _containerValidator = {
+   isOpaque: [
+      k => _isNullish(k)? false : k,
+      _isBoolean,
+   ],
+}
+
+function Container(obj) {
+   obj = _validate(_containerValidator, obj);
+   obj.isContainer = true;
+   return obj;
+}
+
+const _openableValidator = {
+   isOpen: [
+      k => _isNullish(k)? true : k,
+      _isBoolean,
+   ],
+}
+
+function 
+
+// -: parser
+// --------------------------------------------------------------------------------------------- //
+
 
 function parseHub(input) {
    // handling comments
@@ -105,16 +237,88 @@ function parseHub(input) {
       }
    }
    
-   // understand input as possible sentences and parsers
-   const understoods = understand(input);
-   understoods.some((ken, parser) => parser(ken));
-   
-   
+   /* understand input as possible sentences (ken). pass the kenList ot a set of parsers,
+      which will try to understand the input kenList in turn, until one of them
+      understands it and calls an action handler. 
+   */
+
+   const words = CONFIG.matchWordsAndPunc(input);
+   parseHub.parsers.some(parser => parser(words) === true);
    
    say(`I handled what I can so far.`);
    putCmdline();
 }
 
+parseHub.parsers = [];
+
+class Result {
+   constructor(value=null) {
+      this.value = value;
+   }
+}
+
+class Success extends Result {
+   get isSuccess() { 
+      return true;
+   }
+}
+class Failure extends Result { 
+   get isFailure() {
+      return true;
+   }
+}
+class Partial extends Result { 
+   get isPartial() {
+      return true;
+   }
+}
+
+function consume(toConsume, input) {
+   const startOfUnconsumed = 0;
+   for (const ind in toConsume) {
+      if (input[ind] == toConsume[ind]) {
+         startOfUnconsumed += 1;
+      } else {
+         break;
+      }
+   }
+   
+   const consumedNum = startOfUnconsumed;
+      // if unconsumed part starts at index 0, that means 0 tokens were consumed
+   const unconsumedTokens = input.slice(startOfUnconsumed);
+
+   if (toConsume.length == consumedNum) {
+      return new Success(unconsumedTokens);
+   } else if (toConsume.length == 0) {
+      return new Failure(unconsumedTokens);
+   } else if (toConsume.length < consumedNum) {
+      return new Partial(unconsumedTokens);
+   } else {
+      throw new Error();
+   }
+}
+
+
+function consumeStartingWith(toConsume, input) {
+   const startResult = consume(toConsume, input);
+   if (startResult.isSuccess) {
+      const unconsumedTokens = [];
+      return new Success(unconsumedTokens);
+   } else if (startResult.isPartial || startResult.isFailure) {
+      return startResult;
+   } else {
+      throw new Error();
+   }
+}
+
+function parseLukin(words) {
+   // consume o
+   // consume lukin/oko
+   // consume e to end
+   // if e cannot be consumed, consumed tawa to end
+   // if e or tawa cannot be consumed, shout a msg, return failure
+   // parse unconsumed remainder as object
+}
 
 // -: kenlist
 // --------------------------------------------------------------------------------------------- //
@@ -179,97 +383,15 @@ function toKenList(input) {
 }
 
 
-// -: grammar
-// --------------------------------------------------------------------------------------------- //
-
-
-/* take a string that's like "o pali .., e .., tawa .." and turn it into a
-   two-item array (pair tuple). the first of the pair is the pattern, an array
-   of phrases like this: [ ['o', 'pali', null], ['e', null], ['tawa', null] ]
-   the second of the pair is the parser function that will be used on the input
-   if it matches that pattern.
-*/
-function toGrammar(str, parser) {
-   // TODO error handling args
-
-   const pattern = 
-      // split to phrases, like ["o pali ..", "e .."]
-      str
-      .split(/\s*,\s*/)
-      .filter(s => s.length > 0)
-
-      // split to words, and if word is '..' or '...', replace it with null
-      .map(p => {
-         const words = CONFIG.matchWordsAndDots(p);
-         return words.map(w => w.test(/^\.+$/g)? null : w);
-      })
-   ;
-   return [pattern, parser];
-}
-
-/* arg: string input
-   return: [ [ken, parser], [ken, parser], ..]
-*/
-function understand(input) {
-
-   // TODO error handling arg
-
-   function grammarMatchPhrase(phrase, pI, grammar) {
-      const pattern = _fst(grammar);
-
-      for (const wI in phrase) {
-         if (pattern[pI][wI] == phrase[wI]) {
-            continue;
-         } else if (grammar[pI][wI] == null) {
-            return true;
-         } else {
-            return null;
-         }
-      }
-   }
-   function grammarMatchKen(grammar, ken) {
-      return ken.every((phrase, pI) => grammarMatchPhrase(phrase, pI, grammar));
-   }
-
-   const kenList = toKenList(input);
-
-   /* for each ken in kenList, look for the first grammar that matches it. if such exists,
-      add it to an array that will look like this: [ [ken, parser], ..]. 
-   */
-   return (
-      kenList.reduce((accu, ken) => {
-         const grammar = understand.grammars.find(g => grammarMatchKen(g, ken));
-         if (_isDefined(grammar)) {
-            const parser = _snd(grammar);
-            return accu.concat([ [ken, parser] ])
-         } else {
-            return accu;
-         }
-      },
-      accu=[])
-   );
-}
-
-understand.grammars = [];
 
 
 // -: testing
-// ----------------------------------------------------- //
-   
-   
-(function () {
-   console.log(
-      _flattenString(`Hello
-      from
-      the    other*))_)( side    I,. must  , have   called
-      a thousand timesssssssssssssss    `)
-   );
-});
+// --------------------------------------------------------------------------------------------- //
          
 (function testParsing() {
    console.log(
       toKenList('o pana e ijo, tawa jan tawa soweli')
-      )
+   )
 });
    
 (function testMain() {
